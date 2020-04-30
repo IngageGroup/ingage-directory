08826da6a6596b9ba934aed54879bc3185d12954
Michael Stagney <mikestagney@iMac.local>
2020-04-30 13:50:08 -0400

    started to add get employee function for two way binding

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Employee } from '../employees/employee';
+import { DataService } from '../employees/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
+  encapsulation: ViewEncapsulation.Emulated,
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent implements OnInit {
-
-  constructor() { }
+  employeeId = '';
+  employee: Employee;
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) { }
 
   ngOnInit() {
+    const empId = this.route.snapshot.params.id;
+    this.employee = this.dataService.getEmployees().filter(f => f.employeeid === +empId)[0];
   }
 
   submitForm() {
