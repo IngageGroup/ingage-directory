6fb731cfcda4e093f29f1bd275841e9cdb3d1443
Michael Stagney <mikestagney@iMac.local>
2020-05-19 00:02:20 -0400

    edit form fields now complete and appear to work

--- src/app/edit-employee/edit-employee.component.ts
+++ src/app/edit-employee/edit-employee.component.ts
@@ -15,12 +15,6 @@ export class EditEmployeeComponent implements OnInit {
   firstName = '';
   
   profileForm: FormGroup;
-  /*
-  ({
-      firstname: new FormControl('test name '),
-      });*/
-  //console.log(employee.firstname);
-
   
   constructor(
     private route: ActivatedRoute,
@@ -37,18 +31,33 @@ export class EditEmployeeComponent implements OnInit {
     this.profileForm = new FormGroup({
       firstname: new FormControl(this.employee.firstname),
       lastname: new FormControl(this.employee.lastname),
-
+      title: new FormControl(this.employee.title),
+      practicearea: new FormControl(this.employee.practicearea),
+      client: new FormControl(this.employee.client),
+      coach: new FormControl(this.employee.coach),
+      mobilephone: new FormControl(this.employee.mobilephone),
+      email: new FormControl(this.employee.email),
+      champion: new FormControl(this.employee.champion),
+      birthday: new FormControl(this.employee.birthday),
+      anniversary: new FormControl(this.employee.anniversary),
+      hbdiblue: new FormControl(this.employee.hbdiblue),
+      hbdigreen: new FormControl(this.employee.hbdigreen),
+      hbdired: new FormControl(this.employee.hbdired),
+      hbdiyellow: new FormControl(this.employee.hbdiyellow),
+      employeeid: new FormControl(this.employee.employeeid)
     });
   }
 
   acceptChanges() {
     //this will iterate through the fields and update only the fields that changed
-    for (const field in this.employee) {
-      if (this.profileForm.controls[field] !== undefined &&
-        this.profileForm.controls[field].pristine === false) {
-      this.employee[field] = this.profileForm.value[field];
-      console.log('value changed for the ' + field);
-      }
+    
+      for (const field in this.profileForm.value) {
+        if (this.profileForm.controls[field].pristine === false) {
+        this.employee[field] = this.profileForm.value[field];
+        console.log('value changed for the ' + field);
+        }
+
+
     } 
 
   }
